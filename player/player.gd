19e3ee8154d6e0d7cc19b63cdcb81429bdfdf4e7
Caio César Tavares <caiotava@gmail.com>
2024-01-26 23:57:20 +0100

    feat: add initial implementation for player movement

---
+++ player/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export_group("Movement")
+@export var move_speed : float = 220
+@export var move_friction : float = 20
+@export var move_acceleration : float = 60
+
+@onready var animatedSprite = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if velocity != Vector2.ZERO:
+		animatedSprite.play()
+	else:
+		animatedSprite.pause()
+
+func _physics_process(delta):
+	var input := Vector2.ZERO
+	input.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	input.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
+	input = input.normalized()
+
+	if input != Vector2.ZERO:
+		velocity = velocity.move_toward(input * move_speed, move_acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, move_friction * delta)
+
+	if input.length() > 0 :
+		animatedSprite.flip_h = input.x > 0
+
+	move_and_slide()
