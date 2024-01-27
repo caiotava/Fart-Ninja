d6bcb583322f9f2d27e2714f76d3ac3631ba78f6
Aleksandr Movchan <movchan174@gmail.com>
2024-01-28 00:21:16 +0100

    Add game over event connection for passengers

--- main.gd
+++ main.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var passengers = get_tree().get_nodes_in_group("passengers")
+	for passenger in passengers:
+		passenger.connect("game_over", _on_passenger_game_over)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
