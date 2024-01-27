9d44f5f9255ca07ebb0c72e00d9d6f597dbe4b73
Caio César Tavares <caiotava@gmail.com>
2024-01-28 00:01:03 +0100

    feat: create the carriage base scene.

--- train/carriage.gd
+++ train/carriage.gd
@@ -1,17 +1,11 @@
 extends Node2D
 
-@export var paralax_speed : float = -100
-
-@onready var passengers : Node = $Passengers
-@onready var paralax_layer : Sprite2D = $CarriageBackground/BackgroundParalax
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	paralax_layer.region_rect.position.x += paralax_speed * delta
 	pass
 
 func _on_fart_body_entered(body : Node2D):
