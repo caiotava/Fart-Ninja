a6ca25992927d3a1ced4d6d5d77cf6848afadaf0
Caio César Tavares <caiotava@gmail.com>
2024-01-28 02:13:09 +0100

    feat: add train scene with all carriages.

--- train/carriage_base.gd
+++ train/carriage_base.gd
@@ -1,8 +1,11 @@
 extends Node2D
+class_name Carriage
 
 @export var paralax_speed : float = -100
+@export var next_carriage : Carriage = null
+@export var main_camera : Camera2D = null
 
-@onready var passengers : Node = $Passengers
+@onready var start_position : Marker2D = $StartPosition
 @onready var paralax_layer : Sprite2D = $CarriageBackground/BackgroundParalax
 
 # Called when the node enters the scene tree for the first time.
@@ -18,7 +21,10 @@ func _on_fart_body_entered(body : Node2D):
 	if body.is_in_group("passengers"):
 		body.enter_alert_mode()
 
-
 func _on_fart_body_exited(body):
 	if body.is_in_group("passengers"):
 		body.exit_alert_mode()
+
+func _on_end_of_carriage_body_entered(body : Node2D):
+	if body.is_in_group("player") && next_carriage == null:
+		print("Congratulations")
